d112ee57a4f181418624ac39e1d37bb259cd5f6b
qianfei11 <490584635@qq.com>
2021-12-03 13:20:07 +0800

    delete WordCloud & update README

--- Downloader.py
+++ Downloader.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 import requests
 from bs4 import BeautifulSoup
-import numpy as np
-from wordcloud import WordCloud
 import json
 import os
 from tqdm import tqdm
 
-JSON_FILE = 'NDSS21-links.json'
+JSON_FILE = 'NDSS21_links.json'
 FONT = './Comic Sans MS.ttf'
 BLOCK_SZ = 1024
 
@@ -83,42 +81,4 @@ for title, link in papers.items():
             break
         except requests.exceptions.SSLError:
             print '[!] network error, try again'
-
-'''
-title_text = ''
-abstract_text = ''
-for title, abstract in papers.items():
-    try:
-        title_text += title.encode() + ' '
-        abstract_text += abstract.encode() + ' '
-    except UnicodeEncodeError:
-        continue
-
-x, y = np.ogrid[:300, :300]
-mask = (x - 150) ** 2 + (y - 150) ** 2 > 130 ** 2
-mask = 255 * mask.astype(int)
-
-title_wc = WordCloud(background_color='white', repeat=True, mask=mask, font_path=FONT, width=2000, height=2000, scale=4)
-title_wc.generate(title_text)
-title_wc.to_file('./title_cloud.png')
-
-abstract_wc = WordCloud(background_color='white', repeat=True, mask=mask, font_path=FONT, width=2000, height=2000, scale=4)
-abstract_wc.generate(abstract_text)
-abstract_wc.to_file('./abstract_cloud.png')
-'''
-
-'''
-def draw_section_cloud(section_name):
-    text = ''
-    for title, p in papers[section_name].items():
-        try:
-            text += title.encode() + ' '
-            text += p.encode() + ' '
-        except UnicodeEncodeError:
-            continue
-    wc = WordCloud(background_color='white', repeat=True, mask=mask, font_path=FONT, width=2000, height=2000, scale=4)
-    wc.generate(text)
-    section_name = section_name.replace(' ', '_').replace(':', '').replace(';', '').replace(',', '')
-    wc.to_file(section_name + '_cloud.png')
-'''
 
---
+++ README.md
@@ -0,0 +1,31 @@
+# Paper_Downloader_for_NDSS_Symposium_2021
+
+Paper Downloader for NDSS Symposium 2021.
+
+Usage:
+
+```bash
+$ ./Downloader.py
+
+ _______  ________    _________ _________  _______________   ________  ____
+ \      \ \______ \  /   _____//   _____/  \_____  \   _  \  \_____  \/_   |
+ /   |   \ |    |  \ \_____  \ \_____  \    /  ____/  /_\  \  /  ____/ |   |
+/    |    \|    `   \/        \/        \  /       \  \_/   \/       \ |   |
+\____|__  /_______  /_______  /_______  /  \_______ \_____  /\_______ \|___|
+        \/        \/        \/        \/           \/     \/         \/
+________                      .__                    .___
+\______ \   ______  _  ______ |  |   _________     __| _/___________
+ |    |  \ /  _ \ \/ \/ /    \|  |  /  _ \__  \   / __ |/ __ \_  __ \
+ |    `   (  <_> )     /   |  \  |_(  <_> ) __ \_/ /_/ \  ___/|  | \/
+/_______  /\____/ \/\_/|___|  /____/\____(____  /\____ |\___  >__|
+        \/                  \/                \/      \/    \/
+                                                                        Author: B3ale
+
+[+] start downloading <Favocado: Fuzzing the Binding Code of JavaScript Engines Using Semantically Correct Test Cases.pdf>
+100%|██████████| 464k/464k [00:00<00:00, 1.20MiB/s]
+[+] downloaded successfully
+[+] start downloading <CV-Inspector: Towards Automating Detection of Adblock Circumvention.pdf>
+ 84%|████████▍ | 4.04M/4.80M [00:01<00:00, 3.47MiB/s]
+```
+
+
